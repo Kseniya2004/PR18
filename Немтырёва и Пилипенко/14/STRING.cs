7fdff05cb2fd911223e01729c67ad68a61859dad
Kseniya2004 <98638790+Kseniya2004@users.noreply.github.com>
2022-05-25 11:23:15 +0300

    Add files via upload

---
+++ Немтырёва и Пилипенко/14/STRING.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14
+{
+    public class STRING
+    {
+        private string stroka;
+        public string Stroka { get { return stroka; } set { stroka = value; } }
+
+        public STRING() // Конструктор по умолчанию
+        {
+            stroka = "";
+        }
+
+        public STRING(string a)
+        {
+            stroka = a;
+        }
+
+        public string ChangeLine()
+        {
+            string answer = "";
+            char[] myCharArr = stroka.ToCharArray();
+            for (int i = 0; i < myCharArr.Length; i++)
+            {
+                if (char.IsLower(myCharArr[i])) answer += char.ToUpper(myCharArr[i]);
+                else answer += char.ToLower(myCharArr[i]);
+            }
+            return answer;
+        }
+    }
+}
---
+++ Немтырёва и Пилипенко/PR18/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _14;
+
+namespace PR18
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    { 
+        List<string> St = new List<string>()
+        {
+            "Вылечил Яблоко",
+            "Да. Не знаю Зачем, Но Я Его Сделал.",
+            "Влюбился В Пистолет",
+            "Выпил Поезд",
+            "Нарисовал Друга"
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnList_Click(object sender, RoutedEventArgs e)
+        {           
+            foreach(string c in St)
+            {
+                lstTable.Items.Add(c);
+            }
+            
+        }
+
+        private void btnResult_Click(object sender, RoutedEventArgs e)
+        {
+            STRING str = new STRING();
+            for(int i = 0; i < lstTable.Items.Count; i++)
+            {
+                str.Stroka = lstTable.Items[i].ToString();
+                
+                lstTable.Items[i] = str.ChangeLine();
+            }
+            
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            lstTable.Items.Clear();
+        }
+    }
+}
+ 
